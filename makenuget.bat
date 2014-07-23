378232880545e709432d4214364413c16869c768
Peter Ritchie <peter@clear-measure.com>
2014-07-23 13:25:11 -0400

    nuget spec changes

--- makenuget.bat
+++ makenuget.bat
@@ -1,9 +1,7 @@
 @REM TODO: if not exist nuget\lib\net40-client md nuget\lib\net40-client
-@REM TODO: copy src\geschikt\bin\Release\geschikt.dll nuget\lib\net40-client
+@REM TODO: copy Pri.LongPath\bin\Release\Pri.LongPath.dll nuget\lib\net40-client
 if not exist nuget\lib\net45 md nuget\lib\net45
-copy src\Pri.LongPath\bin\Release\gPri.LongPath.dll nuget\lib\net45
-@REM TODO: if not exist nuget\lib\sl4-wp md nuget\lib\sl4-wp 
-@REM TODO: copy src\geschikt.wp7\Bin\Release\geschikt.wp7.dll nuget\lib\sl4-wp 
+copy Pri.LongPath\bin\Release\Pri.LongPath.dll nuget\lib\net45
 
 pushd nuget
 ..\util\nuget.exe pack LongPath.nuspec 
